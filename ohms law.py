51fd7022f8ad0c3c40b2918a442439d4124b7667
jc <joshcahoe@gmail.com>
2025-01-31 05:03:11 -0500

    Added additional functionality

--- ohms law.py
+++ ohms law.py
@@ -1,16 +1,24 @@
 from random import randint, uniform
 from time import sleep
+from os import system
 
 # milliamps_to_amps = ma/1000 <- Just for reference
+# Need to clear the screen after each question
+# Need an Exit Button
+# Need Exceptions Added
+# Random button
+# if you don't add the proper decimals add proper amount of decimals for user
+system('cls')
+print('                    Electronics Calculation Knowledge Quiz')
+sleep(1.2)
+print("                             Developed by JC ")
+sleep(3)
+system('cls')
+
+
 
 class Ohms:
     def __init__(self):
-    #    self.v = randint(1, 12)
-    #    self.r = randint(100, 1000)
-    #    self.i = round(uniform(0.1, 5), 2)
-    #    self.voltage_equation = self.i*self.r
-    #    self.amperage_equation = self.v/self.r
-    #    self.resistance_equation = self.v/self.i
         self.running = True
 
     def volts(self):
@@ -19,6 +27,7 @@ class Ohms:
             self.r = randint(100, 1000)
             self.i = round(uniform(0.1, 5))
             self.voltage_equation = self.i*self.r
+            system('cls')
             print(f'You have {self.i:.2f} amps and {self.r} resistance')
             sleep(1.5)
             print('How many volts does this mean you have?')
@@ -28,11 +37,22 @@ class Ohms:
             if answer == "{:.2f}".format(self.voltage_equation):
                 sleep(1.5)
                 print("Correct! The answer is indeed ","{:.2f}".format(self.voltage_equation))    
+                print('Press any button to continue or type "exit" to leave the program')
+                answer = input()
+                if answer == 'exit':
+                    break
                 sleep(1.5) 
+                system('cls')
+
             else: 
                 sleep(1.5)
                 print('Sorry, that answer is incorrect\nthe correct answer is', "{:.2f}".format(self.voltage_equation) + "\n")
+                print('Press any button to continue or type "exit" to leave the program')
+                answer = input()
+                if answer == 'exit':
+                    break
                 sleep(1.5)
+                system('cls')
         
     def amps(self):
         while self.running:
@@ -40,6 +60,7 @@ class Ohms:
             self.r = round(randint(100, 1000), 2)
             self.i = uniform(0.1, 5)
             self.amperage_equation = self.v/self.r
+            system('cls')
             print(f'You have {self.v} volts and {self.r} resistance')
             sleep(1.5)
             print('How much amperage/current does this mean you have?')
@@ -49,11 +70,21 @@ class Ohms:
             if answer == "{:.4f}".format(self.amperage_equation):
                 sleep(1.5)
                 print("Correct! The answer is indeed ","{:.4f}".format(self.amperage_equation))    
+                print('Press any button to continue or type "exit" to leave the program')
+                answer = input()
+                if answer == 'exit':
+                    break
                 sleep(1.5) 
+                system('cls')
             else: 
                 sleep(1.5)
                 print('Sorry, that answer is incorrect\nthe correct answer is', "{:.4f}".format(self.amperage_equation) + "\n")
+                print('Press any button to continue or type "exit" to leave the program')
+                answer = input()
+                if answer == 'exit':
+                    break
                 sleep(1.5)
+                system('cls')
 
     def res(self):
         while self.running:
@@ -61,6 +92,7 @@ class Ohms:
             self.r = randint(100, 1000)
             self.i = round(uniform(0.1, 5), 4)
             self.resistance_equation = self.v/self.i
+            system('cls')
             print(f'You have {self.v} volts and {self.i:.2f} amperage/current')
             sleep(1.5)
             print('How much resistance does this mean you have?')
@@ -71,23 +103,38 @@ class Ohms:
             if answer == "{:.2f}".format(self.resistance_equation):
                 sleep(1.5)
                 print("Correct! The answer is indeed ","{:.2f}".format(self.resistance_equation))    
+                print('Press any button to continue or type "exit" to leave the program')
+                answer = input()
+                if answer == 'exit':
+                    break
                 sleep(1.5) 
+                system('cls')
             else: 
                 sleep(1.5)
                 print('Sorry, that answer is incorrect\nthe correct answer is', "{:.2f}".format(self.resistance_equation) + "\n")
+                print('Press any button to continue or type "exit" to leave the program')
+                answer = input()
+                if answer == 'exit':
+                    break
                 sleep(1.5)
+                system('cls')
 
-print("Which calculation quiz would you like to take\na) Volts\nb) Amperes)\nc) Reistance)")
-answer = input()
-match answer:
-    case 'a':
-        Ohms().volts()
-    case 'b':
-        Ohms().amps()
-    case 'c':
-        Ohms().res()
-    case _:
-        print('invalid option')
+started = True
+while started:
+    print("Which calculation quiz would you like to take\na) Volts\nb) Amperes)\nc) Reistance)\nd) Exit")
+    answer = input()
+    match answer:
+        case 'a':
+            Ohms().volts()
+        case 'b':
+            Ohms().amps()
+        case 'c':
+            Ohms().res()
+        case 'd':
+            started = False
+        case _:
+            print('invalid option')
+            
 
 
 
